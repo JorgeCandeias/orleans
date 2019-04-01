6e3cdd306e090bcb40f2c73f61818995e5e7be15
Jorge Candeias <candeias@fastmail.fm>
2019-04-01 21:41:06 +0100

    Added state

--- Samples/2.3/SerializationWithBond/src/Grains/SomeGrain.cs
+++ Samples/2.3/SerializationWithBond/src/Grains/SomeGrain.cs
@@ -1,11 +1,17 @@
 using System.Threading.Tasks;
 using Interfaces;
 using Orleans;
+using Interfaces.Models;
 
 namespace Grains
 {
-    public class SomeGrain : Grain, ISomeGrain
+    public class SomeGrain : Grain<SomeState>, ISomeGrain
     {
+        public override Task OnActivateAsync()
+        {
+            return base.OnActivateAsync();
+        }
+
         public Task SomeMethodAsync()
         {
             return Task.CompletedTask;
